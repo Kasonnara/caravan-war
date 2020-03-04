20f45a185ff133f01ab406021f0cde00f3ec8390
Kasonnara <kasonnara.git@laposte.net>
2020-03-04 19:06:06 +0100

    Move Resource classes into their own file

---
+++ common/resources.py
@@ -0,0 +1,37 @@
+class Resources:
+    category = "Resources"
+
+    def __init__(self, quantity):
+        self.quantity = quantity
+
+
+class Gold(Resources):
+    pass
+
+
+class Goods(Resources):
+    pass
+
+
+class Gem(Resources):
+    pass
+
+
+class AmeLegendaire(Resources):
+    pass
+
+
+class LifePotion(Resources):
+    pass
+
+
+class BanditShieldProtection:
+    pass
+
+
+class ReincarnationToken(Resources):
+    pass
+
+
+class Croissance(Resources):
+    pass
